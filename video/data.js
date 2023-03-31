2ca3594c89b44e1cdbffe84d9440fefbb95274ab
sunilkushwaha <sunilkushwaha055>
2023-04-01 03:03:27 +0530

    first commit

---
+++ video/data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'Black Panther: Wakanda Forever',
+        des: 'Queen Ramonda, Shuri, M Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers after the death of beloved King T Challa.',
+        image: '/photo/slider0.webp'
+    },
+    {
+        name: 'loki',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: '/photo/slide 1.png'
+    },
+    {
+        name: 'wanda vision',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: '/photo/slide 2.png'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: '/photo/slide 3.png'
+    },
+    {
+        name: 'luca',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: '/photo/slide 4.png'
+    }
+]
